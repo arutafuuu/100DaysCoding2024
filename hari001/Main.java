1ca231d45c784712c3bbba71eac66a927f0b22d5
Arutafu <arutafuuu@gmail.com>
2024-09-02 22:38:06 +0800

    hari ke 1

---
+++ hari001/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari001;
+
+/**
+ *
+ * @author arutafuuu
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Macam-macam cara menampilkan teks
+ 
+        System.out.print("Ini print ");
+        System.out.print("Halo");
+        System.out.println("Hello World!");
+        
+        String nama = "Altaf";
+        int umur = 20;
+        
+        /*
+        
+        "f" dalam printf mengartikan "format"
+        
+        aljaldladk
+        
+        alkladkl
+        
+        */
+        
+        System.out.printf("Nama: %s, Umur: %d\n", nama, umur);
+        
+    }
+    
+}
